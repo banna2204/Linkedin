0cfeb133fb950408ca3157b230a9f1f08491914c
shubham singh <student@gmail.com>
2025-08-04 04:27:36 +0530

    last commit

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
   credentials:true,
-  origin:"http://localhost:5173"
+  origin:"https://linkedin-clone-60dn.onrender.com"
 }))
 
 app.get('/',(req,res)=>{
